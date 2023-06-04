8832c754b1bf7d5621da4ec9cbe3785e38574c52
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-06-04 13:26:40 +0800

    Added Java code

---
+++ src/main/java/com/helloworld/MyCalculator.java
@@ -0,0 +1,24 @@
+package com.helloworld;
+
+public class MyCalculator {
+
+  public int addition(num1, num2) {
+    output = num1 + num2
+    return output;
+  }
+
+  public int subtraction(num1, num2) {
+    output = num1 - num2
+    return output;
+  }
+
+  public int multiplication(num1, num2) {
+    output = num1 * num2
+    return output;
+  }
+
+  public int division(num1, num2) {
+    output = num1 / num2
+    return output;
+  }
+}
