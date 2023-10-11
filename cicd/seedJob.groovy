7cefe8fff262b2d3a6fc16e1813a08bfe74c887d
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-10-11 21:59:21 +0800

    updated

--- cicd/seedJob.groovy
+++ cicd/seedJob.groovy
@@ -1,4 +1,4 @@
-def folderName = "hello-world"
+def folderName = "devopspilot"
 def pipelineDetails = [
   [name: "01-echo-helloworld", script_path: "cicd/01-Jenkinsfile-helloworld"],
   [name: "02-maven-build", script_path: "cicd/02-Jenkinsfile-maven-build"],
