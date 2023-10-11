10be8d8628aa408725f8cf1317978a9631fefccd
Vignesh Sweekaran <vigneshsweekaran@gmail.com>
2023-10-11 22:06:25 +0800

    updated

--- cicd/seedJob.groovy
+++ cicd/seedJob.groovy
@@ -6,7 +6,10 @@ def pipelineDetails = [
   [name: "04-maven-triggers-hook", script_path: "cicd/04-Jenkinsfile-maven-triggers-hook"],
   [name: "05-maven-triggers-poll", script_path: "cicd/05-Jenkinsfile-maven-triggers-poll"],
   [name: "06-maven-triggers-cron", script_path: "cicd/06-Jenkinsfile-maven-triggers-cron"],
-  [name: "07-maven-build-options", script_path: "cicd/07-Jenkinsfile-maven-build-options"]
+  [name: "07-maven-build-options", script_path: "cicd/07-Jenkinsfile-maven-build-options"],
+  [name: "08-maven-post-cleanup", script_path: "cicd/08-Jenkinsfile-maven-post-cleanup"],
+  [name: "09-maven-build-parameters-string", script_path: "cicd/09-Jenkinsfile-maven-build-parameters-string"],
+  [name: "10-maven-build-parameters-choice", script_path: "cicd/10-Jenkinsfile-maven-build-parameters-choice"]
 ]
 
 folder(folderName) {
@@ -25,99 +28,9 @@ for (pipelineDetail in pipelineDetails) {
             }
           }
         }
+        lightweight(true)
         scriptPath(pipelineDetail.script_path)
       }
     }
   }
 }
-
-// pipelineJob("${folderName}/02-maven-build") {
-//   definition {
-//     cpsScm {
-//       scm{
-//         git {
-//           branch('main')
-//           remote{
-//             url('https://github.com/vigneshsweekaran/hello-world.git')
-//           }
-//         }
-//       }
-//       scriptPath('cicd/02-Jenkinsfile-maven-build')
-//     }
-//   }
-// }
-// pipelineJob("${folderName}/03-maven-build-tools") {
-//   definition {
-//     cpsScm {
-//       scm{
-//         git {
-//           branch('main')
-//           remote{
-//             url('https://github.com/vigneshsweekaran/hello-world.git')
-//           }
-//         }
-//       }
-//       scriptPath('cicd/03-Jenkinsfile-maven-build-tools')
-//     }
-//   }
-// }
-// pipelineJob("${folderName}/04-maven-triggers-hook") {
-//   definition {
-//     cpsScm {
-//       scm{
-//         git {
-//           branch('main')
-//           remote{
-//             url('https://github.com/vigneshsweekaran/hello-world.git')
-//           }
-//         }
-//       }
-//       scriptPath('cicd/04-Jenkinsfile-maven-triggers-hook')
-//     }
-//   }
-// }
-// pipelineJob("${folderName}/05-maven-triggers-poll") {
-//   definition {
-//     cpsScm {
-//       scm{
-//         git {
-//           branch('main')
-//           remote{
-//             url('https://github.com/vigneshsweekaran/hello-world.git')
-//           }
-//         }
-//       }
-//       scriptPath('cicd/05-Jenkinsfile-maven-triggers-poll')
-//     }
-//   }
-// }
-// pipelineJob("${folderName}/06-maven-triggers-cron") {
-//   definition {
-//     cpsScm {
-//       scm{
-//         git {
-//           branch('main')
-//           remote{
-//             url('https://github.com/vigneshsweekaran/hello-world.git')
-//           }
-//         }
-//       }
-//       scriptPath('cicd/06-Jenkinsfile-maven-triggers-cron')
-//     }
-//   }
-// }
-// pipelineJob("${folderName}/07-maven-build-options") {
-//   definition {
-//     cpsScm {
-//       scm{
-//         git {
-//           branch('main')
-//           remote{
-//             url('https://github.com/vigneshsweekaran/hello-world.git')
-//           }
-//         }
-//       }
-//       scriptPath('cicd/07-Jenkinsfile-maven-build-options')
-//     }
-//   }
-// }
